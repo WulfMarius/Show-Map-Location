4d10a4821098c41ba54a189ba7b8c32c027f361b
WulfMarius <WulfMarius@users.noreply.github.com>
2017-12-12 11:40:52 +0100

    fixed an issue that would prevent the location marker from showing when the whole map is revealed

---
+++ src/Patches.cs
@@ -0,0 +1,28 @@
+﻿using Harmony;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace ShowMapLocation
+{
+    [HarmonyPatch(typeof(Panel_Map), "UnloadMapElements")]
+    internal class Panel_Map_UnloadMapElements
+    {
+        public static void Prefix()
+        {
+            ShowMapLocation.HideLocationMarker();
+        }
+    }
+
+    [HarmonyPatch(typeof(Panel_Map), "LoadMapElementsForScene")]
+    internal class Panel_Map_LoadMapElementsForScene
+    {
+        public static void Postfix(Panel_Map __instance, string sceneName)
+        {
+            if (SceneManager.GetActiveScene().name == sceneName)
+            {
+                ShowMapLocation.ShowLocationMarker(sceneName);
+            }
+        }
+    }
+}
---
+++ src/ShowMapLocation.cs
@@ -0,0 +1,65 @@
+﻿using Harmony;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ShowMapLocation
+{
+    internal class ShowMapLocation
+    {
+        private static GameObject locationMarker;
+
+        internal static void HideLocationMarker()
+        {
+            if (locationMarker == null)
+            {
+                return;
+            }
+
+            locationMarker.transform.parent = null;
+            locationMarker.SetActive(false);
+        }
+
+        internal static void ShowLocationMarker(string sceneName)
+        {
+            Traverse traverse = Traverse.Create(InterfaceManager.m_Panel_Map);
+            Vector3 mapPosition = GetWorldPosition(traverse, sceneName);
+
+            MapElementSaveData mapElementSaveData = new MapElementSaveData();
+            mapElementSaveData.m_LocationNameLocID = "GAMEPLAY_Location";
+            mapElementSaveData.m_SpriteName = "ico_X";
+            mapElementSaveData.m_BigSprite = false;
+            mapElementSaveData.m_IsDetail = true;
+            mapElementSaveData.m_NameIsKnown = true;
+            mapElementSaveData.m_PositionOnMap = mapPosition;
+            mapElementSaveData.m_ActiveMissionLocIDs = new List<string>();
+            mapElementSaveData.m_ActiveMissionTimerIDs = new List<string>();
+            mapElementSaveData.m_ActiveMissionIDs = new List<string>();
+
+            if (locationMarker == null)
+            {
+                InitializeLocationMarker(InterfaceManager.m_Panel_Map);
+            }
+            locationMarker.GetComponent<MapIcon>().DoSetup(mapElementSaveData, InterfaceManager.m_Panel_Map.m_DetailEntryActiveObjects, 1000, MapIcon.MapIconType.TopIcon);
+
+            AddToMapData(traverse, mapElementSaveData);
+        }
+
+        private static void AddToMapData(Traverse traverse, MapElementSaveData mapElementSaveData)
+        {
+            Dictionary<Transform, MapElementSaveData> m_TransformToMapData = traverse.Field("m_TransformToMapData").GetValue<Dictionary<Transform, MapElementSaveData>>();
+            m_TransformToMapData.Add(locationMarker.transform, mapElementSaveData);
+        }
+
+        private static Vector3 GetWorldPosition(Traverse traverse, string sceneName)
+        {
+            Traverse worldPositionToMapPosition = traverse.Method("WorldPositionToMapPosition", new System.Type[] { typeof(string), typeof(Vector3) });
+            return worldPositionToMapPosition.GetValue<Vector3>(new object[] { sceneName, GameManager.GetPlayerTransform().localPosition });
+        }
+
+        private static void InitializeLocationMarker(Panel_Map panelMap)
+        {
+            locationMarker = Object.Instantiate<GameObject>(panelMap.m_DetailEntryPrefab);
+            locationMarker.transform.localScale = Vector3.one;
+        }
+    }
+}
